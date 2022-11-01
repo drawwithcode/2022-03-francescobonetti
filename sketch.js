b0d642fb13080eab6c9128e415609ae7a325d8fe
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2022-11-01 22:25:34 +0100

    grafica quasi finita

--- sketch.js
+++ sketch.js
@@ -1,14 +1,34 @@
+let jordanX;
+let jordanY;
+let maloneX;
+let maloneY
+let scale;
+let initialscale;
+
 function preload() {
 	sfondo = loadImage("./assets/sfondo.jpg");
 	jordan = loadImage("./assets/michael.png")
 	basket = loadImage("./assets/basket.png")
+	malone = loadImage("./assets/malone.png")
 }
 
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(0)
+	background(0);
+
+	jordanX = width/2
+	jordanY = height/2
+
+	maloneX = 0;
+	maloneY = 0;
+
+	initialscale = height/jordan.height
+	scale = initialscale*1.1
 	
+}
+
+function draw() {
 	//imposta sfondo
 	backgroundImage(sfondo)
  	 function backgroundImage(img) {
@@ -19,31 +39,45 @@ function setup() {
 		image(img,0,0,img.width*scale,img.height*scale)
 		pop();}
 
-	//micheal
+	//scritta
+
+	textFont("Bebas Neue")
+	textSize(height)
+	fill(255)
+	textAlign(CENTER, CENTER)
+	text("JUMP", width/2, height/2);
 	
-	scale = jordan.height/height
+
+	//micheal e basket
+	
+	image(malone, maloneX, maloneY, malone.width*scale, malone.height*scale)
 
 	push()
 	imageMode(CENTER)
-	translate(width/2, height/2)
-	image(jordan, 0, 0, jordan.width/scale, jordan.height/scale)
+	translate(jordanX, jordanY)
+	image(jordan, 0, 0, jordan.width*scale, jordan.height*scale)
 	pop()
 
-	image(basket, 0, 0, basket.width/scale, basket.height/scale)
-
+	image(basket, 0, 0, basket.width*scale, basket.height*scale)
 	
 
 	
-	
-	
-    
+
+
 }
 
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight)
-  }
+function mouseDragged() {
+	jordanX = mouseX;
+	jordanY = mouseY;
+	
+	let scaleratio = map(mouseX, width, 0, 1, 1.5)
+	scale = initialscale*scaleratio	
+
+	maloneY = map(mouseX, width, 0, 200, -100)
+}
+
+
+
   
 
-function draw() {
-	// put drawing code here
-}
+
