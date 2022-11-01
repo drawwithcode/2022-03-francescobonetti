7c8487cd4cb4f64d56ca0c569d68716c6e953419
francescobonetti <84150908+francescobonetti@users.noreply.github.com>
2022-11-01 20:10:54 +0100

    Impostazione base

--- sketch.js
+++ sketch.js
@@ -1,15 +1,48 @@
 function preload() {
-	// put preload code here
+	sfondo = loadImage("./assets/sfondo.jpg");
+	jordan = loadImage("./assets/michael.png")
+	basket = loadImage("./assets/basket.png")
 }
+
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+	background(0)
+	
+	//imposta sfondo
+	backgroundImage(sfondo)
+ 	 function backgroundImage(img) {
+		push();
+		translate(width/2,height/2);
+		imageMode(CENTER);
+		let scale = Math.max(width/img.width,height/img.height);
+		image(img,0,0,img.width*scale,img.height*scale)
+		pop();}
+
+	//micheal
+	
+	scale = jordan.height/height
+
+	push()
+	imageMode(CENTER)
+	translate(width/2, height/2)
+	image(jordan, 0, 0, jordan.width/scale, jordan.height/scale)
+	pop()
+
+	image(basket, 0, 0, basket.width/scale, basket.height/scale)
+
+	
+
+	
+	
+	
+    
 }
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight)
+  }
+  
 
 function draw() {
 	// put drawing code here
